dcfc4785335857a12961bdff19ef93b972c95817
sunanmau5 <sunan.regi@gmail.com>
2026-01-28 18:27:09 +0100

    feat(nvim): transparent lualine background

--- lua/plugins/lualine.lua
+++ lua/plugins/lualine.lua
@@ -1,28 +1,34 @@
 return {
 	"nvim-lualine/lualine.nvim",
 	dependencies = { "nvim-tree/nvim-web-devicons" },
-	opts = {
-		options = {
-			theme = "ayu_mirage",
-			component_separators = "",
-			section_separators = "",
-			disabled_filetypes = { statusline = { "neo-tree" } },
-		},
-		sections = {
-			lualine_a = { "mode" },
-			lualine_b = { "branch", "diff" },
-			lualine_c = { { "filename", path = 1 } },
-			lualine_x = {},
-			lualine_y = {},
-			lualine_z = {},
-		},
-		inactive_sections = {
-			lualine_a = {},
-			lualine_b = {},
-			lualine_c = {},
-			lualine_x = {},
-			lualine_y = {},
-			lualine_z = {},
-		},
-	},
+	opts = function()
+		local theme = require("lualine.themes.ayu_mirage")
+		for _, mode in pairs(theme) do
+			if mode.c then mode.c.bg = "NONE" end
+		end
+		return {
+			options = {
+				theme = theme,
+				component_separators = "",
+				section_separators = "",
+				disabled_filetypes = { statusline = { "neo-tree" } },
+			},
+			sections = {
+				lualine_a = { "mode" },
+				lualine_b = { "branch", "diff" },
+				lualine_c = { { "filename", path = 1 } },
+				lualine_x = {},
+				lualine_y = {},
+				lualine_z = {},
+			},
+			inactive_sections = {
+				lualine_a = {},
+				lualine_b = {},
+				lualine_c = {},
+				lualine_x = {},
+				lualine_y = {},
+				lualine_z = {},
+			},
+		}
+	end,
 }
