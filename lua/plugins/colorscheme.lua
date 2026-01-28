237fede46a5c36793caab0102a0607e87df21772
sunanmau5 <sunan.regi@gmail.com>
2026-01-28 18:27:06 +0100

    feat(nvim): add transparent background overrides

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -2,8 +2,37 @@ return {
 	"Shatur/neovim-ayu",
 	lazy = false,
 	priority = 1000,
-	config = function()
-		require("ayu").setup({ mirage = true })
+	opts = {
+		mirage = true,
+		overrides = {
+			Normal = { bg = "NONE" },
+			NormalNC = { bg = "NONE" },
+			NormalFloat = { bg = "NONE" },
+			FloatBorder = { bg = "NONE" },
+			-- line numbers (brighter for visibility on transparent bg)
+			LineNr = { fg = "#5c6773" },
+			CursorLineNr = { fg = "#ffcc66" },
+			-- statusline
+			StatusLine = { bg = "NONE" },
+			StatusLineNC = { bg = "NONE" },
+			-- neo-tree
+			NeoTreeNormal = { bg = "NONE" },
+			NeoTreeNormalNC = { bg = "NONE" },
+			NeoTreeWinSeparator = { fg = "NONE", bg = "NONE" },
+			WinSeparator = { fg = "NONE", bg = "NONE" },
+			NeoTreeEndOfBuffer = { fg = "NONE", bg = "NONE" },
+			EndOfBuffer = { fg = "NONE" },
+			-- gitsigns
+			GitSignsAdd = { bg = "NONE" },
+			GitSignsChange = { bg = "NONE" },
+			GitSignsDelete = { bg = "NONE" },
+			SignColumn = { bg = "NONE" },
+			-- bufferline / tabline
+			TabLineFill = { bg = "NONE" },
+		},
+	},
+	config = function(_, opts)
+		require("ayu").setup(opts)
 		vim.cmd.colorscheme("ayu")
 	end,
 }
