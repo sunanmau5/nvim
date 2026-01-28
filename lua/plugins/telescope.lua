4f75aa19aecdcb74d9de9e3874b5e83f3d63d4e4
sunanmau5 <sunan.regi@gmail.com>
2026-01-28 18:27:12 +0100

    feat(nvim): add fzf and frecency telescope extensions

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -1,21 +1,40 @@
 return {
 	"nvim-telescope/telescope.nvim",
-	dependencies = { "nvim-lua/plenary.nvim" },
+	dependencies = {
+		"nvim-lua/plenary.nvim",
+		{ "nvim-telescope/telescope-fzf-native.nvim", build = "make" },
+		{ "nvim-telescope/telescope-frecency.nvim", version = "*" },
+	},
+	config = function(_, opts)
+		require("telescope").setup(opts)
+		require("telescope").load_extension("fzf")
+		require("telescope").load_extension("frecency")
+	end,
 	cmd = "Telescope",
 	keys = {
-		{ "<leader><leader>", "<cmd>Telescope find_files hidden=true<cr>" },
+		{ "<C-p>", "<cmd>Telescope frecency workspace=CWD<cr>" },
+		{ "<leader><leader>", "<cmd>Telescope frecency workspace=CWD<cr>" },
 		{ "<leader>/", "<cmd>Telescope live_grep<cr>" },
+		{ "<leader>/", function() require("telescope.builtin").live_grep({ default_text = vim.fn.expand("<cword>") }) end, mode = "v" },
 		{ "<leader>fb", "<cmd>Telescope buffers<cr>" },
 	},
-	opts = {
-		defaults = {
-			layout_strategy = "horizontal",
-			layout_config = { prompt_position = "top" },
-			sorting_strategy = "ascending",
-		},
-		pickers = {
-			find_files = { hidden = true },
-			live_grep = { additional_args = { "--hidden" } },
-		},
-	},
+	opts = function()
+    local actions = require("telescope.actions")
+    return {
+      defaults = {
+        layout_strategy = "horizontal",
+        layout_config = { prompt_position = "top" },
+        sorting_strategy = "ascending",
+        mappings = {
+          i = {
+            ["<esc>"] = actions.close
+          }
+        }
+      },
+      pickers = {
+        find_files = { hidden = true },
+        live_grep = { additional_args = { "--hidden" } },
+      },
+    }
+  end
 }
