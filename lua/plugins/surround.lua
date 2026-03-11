63229788adf8fb8c492094a06776ad01b24caa0c
sunanmau5 <sunan.regi@gmail.com>
2026-03-11 15:04:18 +0100

    feat(nvim): add nvim-surround plugin

---
+++ lua/plugins/surround.lua
@@ -0,0 +1,10 @@
+return {
+  "kylechui/nvim-surround",
+  version = "*",
+  event = "VeryLazy",
+  opts = {},
+  keys = {
+    { "gs", "<Plug>(nvim-surround-visual)", mode = "x", desc = "Surround visual selection" },
+    { "gS", "<Plug>(nvim-surround-visual-line)", mode = "x", desc = "Surround visual selection (line)" },
+  },
+}
