8e1a8d6ae1b689d38ebff69280b17abed7108812
sunanmau5 <sunan.regi@gmail.com>
2026-03-11 15:04:41 +0100

    feat(nvim/obsidian): add icloud workspace, conceallevel callbacks

--- lua/plugins/obsidian.lua
+++ lua/plugins/obsidian.lua
@@ -7,6 +7,16 @@ return {
 	opts = {
 		workspaces = {
 			{ name = "vault", path = "~/Documents/obsidian/vault" },
+			{ name = "cloud-storage", path = "~/Library/Mobile Documents/iCloud~md~obsidian/Documents/cloud-storage" },
+		},
+		disable_frontmatter = true,
+		callbacks = {
+			enter_note = function()
+				vim.opt.conceallevel = 2
+			end,
+			leave_note = function()
+				vim.opt.conceallevel = 0
+			end,
 		},
 	},
 }
