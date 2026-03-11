c1c40f8791bf0e48d013945700a298fdd6a3cb97
sunanmau5 <sunan.regi@gmail.com>
2026-03-11 15:04:15 +0100

    feat(nvim): add blink.cmp completion plugin

---
+++ lua/plugins/blink.lua
@@ -0,0 +1,18 @@
+return {
+	"saghen/blink.cmp",
+	version = "*", -- use pre-built Rust binaries, no toolchain required
+	opts = {
+		keymap = {
+			preset = "default",
+			["<CR>"] = { "accept", "fallback" },
+		}, -- <C-space> trigger, <CR> accept, <C-e> close
+		appearance = { nerd_font_variant = "mono" },
+		completion = {
+			accept = { auto_brackets = { enabled = true } },
+			documentation = { auto_show = true, auto_show_delay_ms = 200 },
+		},
+		sources = {
+			default = { "lsp", "path", "buffer" },
+		},
+	},
+}
