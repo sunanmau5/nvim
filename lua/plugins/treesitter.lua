b0b3616263d1d26b2e1e057ec9512e6c5da6912a
sunanmau5 <sunan.regi@gmail.com>
2026-03-21 18:37:15 +0100

    fix(nvim/treesitter): invalid config

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -1,16 +1,30 @@
 return {
     "nvim-treesitter/nvim-treesitter",
+    lazy = false,
     build = ":TSUpdate",
     dependencies = {
         "nvim-treesitter/nvim-treesitter-textobjects",
     },
     config = function()
-        require("nvim-treesitter.configs").setup({
-            ensure_installed = {
-                "javascript",
-                "typescript",
-                "tsx",
-                "json",
+        local treesitter = require("nvim-treesitter")
+        treesitter.setup()
+        treesitter.install({
+            "lua",
+            "ruby",
+            "sql",
+            "markdown",
+            "markdown_inline",
+            "python",
+            "vim",
+            "vimdoc",
+            "javascript",
+            "typescript",
+            "tsx",
+            "json",
+        })
+
+        vim.api.nvim_create_autocmd("FileType", {
+            pattern = {
                 "lua",
                 "ruby",
                 "sql",
@@ -19,8 +33,20 @@ return {
                 "python",
                 "vim",
                 "vimdoc",
+                "javascript",
+                "typescript",
+                "tsx",
+                "json",
             },
-            highlight = { enable = true },
+            callback = function()
+                -- syntax highlighting, provided by Neovim
+                vim.treesitter.start()
+                -- folds, provided by Neovim (I don't like folds)
+                -- vim.wo.foldexpr = 'v:lua.vim.treesitter.foldexpr()'
+                -- vim.wo.foldmethod = 'expr'
+                -- indentation, provided by nvim-treesitter
+                vim.bo.indentexpr = "v:lua.require'nvim-treesitter'.indentexpr()"
+            end,
         })
     end,
 }
