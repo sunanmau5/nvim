7605ad0062b774850b4ba252592a5b1d6ea51147
sunanmau5 <sunan.regi@gmail.com>
2026-04-23 10:43:19 +0200

    fix: vue lsp setup

--- lua/lsp/init.lua
+++ lua/lsp/init.lua
@@ -1,6 +1,6 @@
 local vue_language_server_path = vim.fn.stdpath("data")
     .. "/mason/packages/vue-language-server/node_modules/@vue/language-server"
-local tsserver_filetypes = { "javascript", "javascriptreact", "typescript", "typescriptreact", "vue", "json" }
+local tsserver_filetypes = { "typescript", "javascript", "javascriptreact", "typescriptreact", "vue" }
 local vue_plugin = {
     name = "@vue/typescript-plugin",
     location = vue_language_server_path,
@@ -41,6 +41,39 @@ vim.lsp.config("vue_ls", {
     cmd = { "vue-language-server", "--stdio" },
     filetypes = { "vue" },
     root_markers = { "package.json", "tsconfig.json", "jsconfig.json", ".git" },
+    on_init = function(client)
+        client.handlers["tsserver/request"] = function(_, result, context)
+            local ts_clients = vim.lsp.get_clients({ bufnr = context.bufnr, name = "ts_ls" })
+            local vtsls_clients = vim.lsp.get_clients({ bufnr = context.bufnr, name = "vtsls" })
+            local clients = {}
+            vim.list_extend(clients, ts_clients)
+            vim.list_extend(clients, vtsls_clients)
+
+            if #clients == 0 then
+                vim.notify(
+                    "Could not find `vtsls` or `ts_ls` lsp client, `vue_ls` would not work without it.",
+                    vim.log.levels.ERROR
+                )
+                return
+            end
+            local ts_client = clients[1]
+            local param = unpack(result)
+            local id, command, payload = unpack(param)
+            ts_client:exec_cmd({
+                title = "vue_request_forward",
+                command = "typescript.tsserverRequest",
+                arguments = {
+                    command,
+                    payload,
+                },
+            }, { bufnr = context.bufnr }, function(_, r)
+                local response = r and r.body
+                local response_data = { { id, response } }
+                ---@diagnostic disable-next-line: param-type-mismatch
+                client:notify("tsserver/response", response_data)
+            end)
+        end
+    end,
 })
 
 vim.lsp.config("lua_ls", {
